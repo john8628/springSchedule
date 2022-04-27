438f512cd9e0dd534ac9235144e1592d66d04a42
wenwei <zhouwenwei@Jwenwei-MacBook-Pro.local>
2022-04-27 10:20:41 +0800

    add leetcode

---
+++ src/main/java/com/johj/leetcode/DeleteDuplicateInArray.java
@@ -0,0 +1,49 @@
+package com.johj.leetcode;
+
+
+/**
+ *
+ * https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2gy9m/
+ *
+ * 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。
+ *
+ * 由于在某些语言中不能改变数组的长度，所以必须将结果放在数组nums的第一部分。更规范地说，如果在删除重复项之后有 k 个元素，那么 nums 的前 k 个元素应该保存最终结果。
+ *
+ * 将最终结果插入 nums 的前 k 个位置后返回 k 。
+ *
+ * 示例 1：
+ * 输入：nums = [1,1,2]
+ * 输出：2, nums = [1,2,_]
+ * 解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+ * 示例 2：
+ *
+ * 输入：nums = [0,0,1,1,1,2,2,3,3,4]
+ * 输出：5, nums = [0,1,2,3,4]
+ * 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
+ *
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2gy9m/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ *
+
+ */
+
+public class DeleteDuplicateInArray {
+  public static int removeDuplicates(int[] nums) {
+    //数组长度
+    int length = nums.length;
+    int i = 0  ;
+    for (int j = 1 ; j < length ; j++){
+      if(nums[i]!= nums[j]){
+        nums[++i] = nums[j];
+      }
+    }
+    System.out.println(nums);
+    return i+1;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(removeDuplicates(new int[]{1,2,3,4,5,6}));
+  }
+}
